d276c2b43a2d95a0ca42e7c9e466e46cbe48495d
b_g <b_g@dsm.hs.kr>
2024-10-11 16:24:23 +0900

    chore: change review_model

--- lib/models/review_model.dart
+++ lib/models/review_model.dart
@@ -1,15 +1,15 @@
 class ReviewModel {
   final int id;
-  final int userId;
+  final String name;
   final String content;
   final double star;
 
-  const ReviewModel({required this.id, required this.userId, required this.content, required this.star});
+  const ReviewModel({required this.id, required this.name, required this.content, required this.star});
 
   factory ReviewModel.fromJson(Map<String, dynamic> json) {
     return ReviewModel(
       id: json['review_id'],
-      userId: json['user_id'],
+      name: json['user_name'],
       content: json['content'],
       star: json['star']
     );
