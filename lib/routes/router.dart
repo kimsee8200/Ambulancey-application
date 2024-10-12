1069f949e0b6583fee88c8024ea8fb3cdc56fe93
b_g <b_g@dsm.hs.kr>
2024-10-12 22:06:22 +0900

    chore: create WidgetTest page

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,7 +1,9 @@
+import 'package:ambulancey/views/widget_test.dart';
 import 'package:ambulancey/views/user/login.dart';
 import 'package:ambulancey/views/user/signup.dart';
 
 final router = {
+  '/test': (context) => const WidgetTest(),
   '/login': (context) => const Login(),
   '/signup': (context) => const Signup()
 };
---
+++ lib/views/widget_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WidgetTest extends StatefulWidget {
+  const WidgetTest({super.key});
+
+  @override
+  State<WidgetTest> createState() => _WidgetTestState();
+}
+
+class _WidgetTestState extends State<WidgetTest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
