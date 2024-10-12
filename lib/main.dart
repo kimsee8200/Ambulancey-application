4bf4c35cbacdb93357a4b4ecbbc1159144b56276
b_g <b_g@dsm.hs.kr>
2024-10-13 00:28:44 +0900

    chore: route to test when kDebugMode

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ambulancey/routes/router.dart';
 import 'package:ambulancey/views/common/color.dart';
 
 void main() => runApp(
   MaterialApp(
-    initialRoute: '/login',
+    initialRoute: kDebugMode ? '/test' : '/login',
     routes: router,
     theme: ThemeData(
       fontFamily: 'Pretendard',
